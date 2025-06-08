d7d35e1bdcf7e5aea0897aacf05ffbc0b089ca74
Sergey Klochkov <klochkov_2003@list.ru>
2025-06-08 05:09:40 +0400

    fixed main.cpp for launch

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,6 @@ int main(int argc, char** argv)
 {
     generator::Parser parser("/home/sklochkov/ritm-test/data/scheme.xml");
     generator::Generator code_generator(parser.parse());
-    code_generator.generate_code("my_struct", "my_struct");
+    code_generator.generate_code();
     return 0;
 }
